a829b9dd641209ebc4f45e76ce318d2e2dcb550f
Valeriy Smolskiy <valeriy.smolskiy@roonyx.tech>
2023-05-19 19:48:05 +0300

    feat(dnd): add custom render for component

--- apps/components-library/src/app/app.component.ts
+++ apps/components-library/src/app/app.component.ts
@@ -1,13 +1,61 @@
-import { Component } from '@angular/core';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {
+  ApplicationRef,
+  ChangeDetectionStrategy,
+  Component,
+  createComponent,
+  Type,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CharacterFormContainerComponent } from '@dnd-masters/character-form';
 
+export interface ComponentItem {
+  name: string;
+  class: Type<unknown>;
+}
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent],
   selector: 'dnd-masters-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'components-library';
+  @ViewChild('selectedComponent', { read: ViewContainerRef, static: true })
+  selectedComponent!: ViewContainerRef;
+
+  public data: ComponentItem[] = [
+    {
+      name: 'Component1',
+      class: CharacterFormContainerComponent,
+    },
+    {
+      name: 'Component2',
+      class: CharacterFormContainerComponent,
+    },
+    {
+      name: 'Component3',
+      class: CharacterFormContainerComponent,
+    },
+    {
+      name: 'Component4',
+      class: CharacterFormContainerComponent,
+    },
+    {
+      name: 'Component5',
+      class: CharacterFormContainerComponent,
+    },
+  ];
+
+  constructor(private readonly applicationRef: ApplicationRef) {}
+
+  public componentClicked(item: ComponentItem): void {
+    const component = createComponent(item.class, {
+      environmentInjector: this.applicationRef.injector,
+    });
+    this.selectedComponent.clear();
+    this.selectedComponent.insert(component.hostView);
+  }
 }
--- libs/character-form/src/index.ts
+++ libs/character-form/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/character-form.module';
+export * from './lib/containers/character-form-container/character-form-container.component';
